5cae40159a564f13e859fc044349e2c8c4f80bea
Tolulope Amole <amoletolulope@gmail.com>
2022-12-19 14:42:07 +0100

    policy and T&C js added

--- js/T&C.js
+++ js/T&C.js
@@ -1,3 +1,8 @@
-// if(!localStorage.getItem("remember")){
-//     window.location.replace("./login.html")
-// }
+const generate = document.getElementById("generate")
+generate.addEventListener("click", ()=>{
+  if(!localStorage.getItem("auth") || !sessionStorage.getItem("auth")){
+    window.location.href = "./login.html"
+  } else{
+    window.location.href = "./dashboard/termsgenerator.html"
+  }
+})
--- js/privacy-policy.js
+++ js/privacy-policy.js
@@ -1,3 +1,11 @@
 // if(!localStorage.getItem("remember")){
 //     window.location.replace("./login.html")
 // }
+const generate = document.getElementById("generate")
+generate.addEventListener("click", ()=>{
+  if(!localStorage.getItem("auth") || !sessionStorage.getItem("auth")){
+    window.location.href = "./login.html"
+  } else{
+    window.location.href = "./dashboard/privacypolicygenerator.html"
+  }
+})
